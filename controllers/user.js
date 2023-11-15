c157b5c86df5a7c404f31226aeb63b5479385593
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-15 12:41:30 +0500

    create hashpassword

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,9 @@
  const User = require("../models/user");
+ const bcrypt = require("bcrypt");
  const createUser = async (data) => {
     try{
-        const user = new User(data);
+        const hashPassword = await bcrypt.hash(data.password, 12)
+        const user = new User({...data, password: hashPassword});
          await user.save();
          return "User Created Successfully"
     }catch(err){
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,8 @@ const { createUser } = require("../controllers/user");
 // user Create Controller
 router.post("/",  async (req, res) => {
   try{
-    const response = await createUser(req.body);
+    const {name, email, phone, password} = req.body;
+    const response = await createUser({name, email, phone, password});
    return res.status(200).send({status:200, message: response});
   }catch(err){
    return res.status(400).send({status:400, message:err.message});
