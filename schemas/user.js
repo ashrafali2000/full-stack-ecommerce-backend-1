73c3a849a5ee5024765465a6ec31e7238ca3fc23
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-15 10:37:40 +0500

    create user schema

---
+++ schemas/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+ const UserSchema  = new Schema({
+    name:{
+        type:Schema.Types.String,
+        require:true
+    },
+    email:{
+        type:Schema.Types.String,
+        require:true,
+        unique:true
+    },
+    phone:{
+        type:Schema.Types.Number,
+        require:true
+    },
+    password:{
+        type:Schema.Types.String,
+        require:true
+    }
+ })
+
+ module.exports = UserSchema
