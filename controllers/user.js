64fa4be8fcf497f068d7c37528f072040f5befef
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-11-15 14:48:30 +0500

    login code

--- controllers/user.js
+++ controllers/user.js
@@ -1,29 +1,37 @@
- const User = require("../models/user");
- const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 //  Create User
- const createUser = async (data) => {
-    try{
-        const hashPassword = await bcrypt.hash(data.password, 12)
-        const user = new User({...data, password: hashPassword});
-         await user.save();
-         return "User Created Successfully"
-    }catch(err){
-        return "User Already Exist";
-    }
- }
+const createUser = async (data) => {
+  try {
+    const hashPassword = await bcrypt.hash(data.password, 12);
+    const user = new User({ ...data, password: hashPassword });
+    await user.save();
+    return "User Created Successfully";
+  } catch (err) {
+    return "User Already Exist";
+  }
+};
 
 // Login User
- const loginUser = async (data) => {
-    try{
-        const found = await User.findOne({email:data.email});
-        if(found){
+const loginUser = async (data) => {
+  try {
+    const { email, password } = data;
+    const userFound = await User.findOne({ email });
+    if (userFound) {
+      const comparePassword = await bcrypt.compare(
+        password,
+        userFound.password
+      );
+      if (comparePassword) {
         return "Login Successful";
-        } else{
-            throw new Error("User not Found");
-        }
-    }catch(err){
-      throw err;
+      } else if (!comparePassword) {
+        throw new Error("Incorrect Password");
+      }
     }
- }
-module.exports = {createUser, loginUser};
+    throw new Error("User not Found");
+  } catch (err) {
+    throw err;
+  }
+};
+module.exports = { createUser, loginUser };
