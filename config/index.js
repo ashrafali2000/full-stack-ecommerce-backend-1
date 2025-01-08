8a5fecd6fb4e5e1a09032195f75c638a07126260
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2025-01-08 11:31:45 +0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
-const password = process.env.PASSWORD
- mongoose.connect(`mongodb+srv://Ashrafali:${password}@ashratech.fbnrqrf.mongodb.net/full-stack-Ecommerce-Backend-1?retryWrites=true&w=majority`)
+const password = process.env.DB_URI
+ mongoose.connect(password)
 module.exports = mongoose
