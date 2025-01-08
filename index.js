7dea3331aed96a2a8d62bbdff32598b128a73535
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2025-01-08 11:24:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,23 @@ const app = express();
 app.use(cors());
 const port = 8000;
 app.use(express.json());
-
 // const bodyParser = require("body-parser")
 // app.use(bodyParser.urlencoded({extends:false}))
 // const path = require("path");
 // app.use(express.static(path.join(process.cwd(), "public")))
-
-
-// Products MiddleWare
-app.use("/products", product);
-
-// Users MiddleWare
-app.use("/user", user);
-
 // db CONNECTION
 const db = mongoose.connection;
 db.on("error",console.error.bind( console, "Connection Error"));
 db.once("open", function (){
   console.log(chalk.bgGreen("db Connected"));
 })
-
+app.get("/", (req, res) => {
+  res.send("Server is running....");
+});
+// Products MiddleWare
+app.use("/products", product);
+// Users MiddleWare
+app.use("/user", user);
 app.listen(port, (req, res) => {
   console.log(`Server listen on Port ${port}`);
 });
